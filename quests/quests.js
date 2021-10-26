b4ba2545581e05497d8d1d2791efb04e76f0e2a3
James Demiraiakian <james.armen@gmail.com>
2021-10-26 13:56:03 -0700

    added some functionality to quests page

--- quests/quests.js
+++ quests/quests.js
@@ -1,1 +1,22 @@
-console.log('Welcome to Quests');
+import quests from '../quest-data/quest-data.js';
+import { findByID, getChar, setChar } from '../functions.js';
+
+const parameters = new URLSearchParams(window.location.search);
+
+console.log(parameters.get('id'));
+
+const questData = findByID(parameters.get('id'), quests);
+
+console.log(questData);
+
+const questTitle = document.getElementById('quest-title');
+questTitle.textContent = questData.title;
+
+const questImg = document.getElementById('quest-img');
+questImg.src = questData.image;
+questImg.alt = questData.title;
+
+const description = document.getElementById('description');
+description.textContent = questData.description;
+
+
